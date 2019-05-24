3268ff4821e45c0069b708d09cdbc3e2b596122f
root <bakmen2543@gmail.com>
2019-05-24 12:41:44 +0700

    Прочее

--- bot/modules/system/ver.py
+++ bot/modules/system/ver.py
@@ -1,4 +1,4 @@
 import bot.classes.pyTelegramApi as api
 
 def main():
-	api.pyTelegramApi.sendMessage_array(api, ['Привет', 'Привет23'])
+	api.pyTelegramApi.sendMessage_array(api, ['/start - Возвращаем стэк команд', '/getChatId - Возвращаем chat_id', '/getStickerId -  Возвращаем стикер ид', '/sendSticker - Отправить стикер по ид', '/ava - Отправить аватарку', '/help - Возвращаем стэк команд'])
